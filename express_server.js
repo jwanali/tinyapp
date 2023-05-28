2dc95d2f91fbee057653b8a4bab63aa09a003bd5
jwanali <ciwan_ali@hotmail.com>
2023-05-27 22:31:21 -0500

    Add post for form submission

--- express_server.js
+++ express_server.js
@@ -1,31 +1,48 @@
-const express = require('express');
- const app = express();
- const PORT = 8080; // default port 8080
+const express = require("express");
+const app = express();
+const PORT = 8080; // default port 8080
 
- app.set("view engine", "ejs")
+app.set("view engine", "ejs");
 
+app.use(express.urlencoded({ extended: true }));
+function generateRandomString() {
+    const chars = '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ';
+    let result = '';
+    for (let i = 0; i < 6; i++) {
+      result += chars[Math.floor(Math.random() * chars.length)];
+    } 
+    return result;
+}
 
- const urlDatabase = {
+const urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
-  "9sm5xK": "http://www.google.com"
+  "9sm5xK": "http://www.google.com",
 };
-
-
+app.post("/urls", (req, res) => {
+  console.log(req.body); // Log the POST request body to the console
+  res.send(`ok ${generateRandomString()}`); // Respond with 'Ok' (we will replace this)
+});
 app.get("/", (req, res) => {
   res.send("Hello!");
 });
-app.get("/urls.json", (req,res) => {
+app.get("/urls.json", (req, res) => {
   res.json(urlDatabase);
-})
+});
 app.get("/hello", (req, res) => {
   res.send("<html><body>Hello <b>World</b></body></html>\n");
 });
-app.get("/urls", (req,res) => {
+app.get("/urls", (req, res) => {
   const templateVars = { urls: urlDatabase };
-  res.render("urls_index", templateVars)
+  res.render("urls_index", templateVars);
+});
+app.get("/urls/new", (req, res) => {
+  res.render("urls_new");
 });
 app.get("/urls/:id", (req, res) => {
-  const templateVars = { id: req.params.id, longURL: urlDatabase[req.params.id] };
+  const templateVars = {
+    id: req.params.id,
+    longURL: urlDatabase[req.params.id],
+  };
   res.render("urls_show", templateVars);
 });
 
