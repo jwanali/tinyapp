e723a0afaa1ee074f209243afa6b7ef2be548309
jwanali <ciwan_ali@hotmail.com>
2023-05-31 19:41:45 -0500

    Refactoring the Login Route

--- express_server.js
+++ express_server.js
@@ -23,6 +23,7 @@ const generateRandomString = function (length) {
 
 const getUserByEmail = function (email) {
   for (user in users) {
+    
     if (users[user].email === email) {
       return users[user];
     }
